5d0a0fbd51841faa3fb2981f50b1a4b13deadabe
Vagner Bernal de castro <vagner.castro@objective.com.br>
2020-10-20 17:03:10 -0300

    Create user

---
+++ backend/src/controllers/UsersController.ts
@@ -0,0 +1,59 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import bcrypt from "bcrypt";
+import * as Yup from "yup";
+
+import UserView from "../views/users_view";
+import User from "../models/User";
+
+export default {
+  async index(req: Request, res: Response) {
+    const repository = getRepository(User);
+
+    const users = await repository.find();
+
+    res.status(200).send(UserView.renderMany(users));
+  },
+
+  async create(req: Request, res: Response) {
+    const { name, email, password } = req.body;
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().required(),
+      password: Yup.string().required(),
+    });
+
+    await schema.validate(
+      {
+        name,
+        email,
+        password,
+      },
+      {
+        abortEarly: false,
+      }
+    );
+
+    const repository = getRepository(User);
+
+    const userCreated = await repository.findOne({ email });
+
+    if (userCreated)
+      return res.status(400).json({ error: "User already exists." });
+
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = repository.create({
+      name,
+      email,
+      password: hash,
+    });
+
+    repository.save(user);
+
+    return res.status(201).json(user);
+  },
+};
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,6 +3,8 @@ import multer from "multer";
 
 import uploadConfig from "./config/upload";
 import OrphanagesController from "./controllers/OrphanatesController";
+import UsersController from "./controllers/UsersController";
+import AuthenticationController from "./controllers/AuthenticationController";
 
 const router = Router();
 const upload = multer(uploadConfig);
@@ -10,6 +12,11 @@ const upload = multer(uploadConfig);
 router.get("/", (req, res) => {
   res.json({ message: "Happy" });
 });
+
+router.post("/users", UsersController.create);
+router.get("/users", UsersController.index);
+
+router.post("/authenticate", AuthenticationController.authenticate);
 
 router.get("/orphanages", OrphanagesController.index);
 router.get("/orphanages/:id", OrphanagesController.show);
